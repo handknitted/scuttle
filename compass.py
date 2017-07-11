0de8fe1ba7b949c2d8c3e054a1701a17130bc4bb
David C Kennedy <darfed@gmail.com>
2017-07-11 20:36:54 +0000

    Added which_way_to_bearing in compass

--- compass.py
+++ compass.py
@@ -1,13 +1,12 @@
 import time
 import XLoBorg
-from math import cos,sin,pi, atan2, floor, asin
-
+from math import cos, sin, pi, atan2, floor, asin
 
 
 class Compass:
 
     MAGNETIC_NORTH = 0
-    BRISTOL = 0  #-2.2
+    BRISTOL = -1
     declinationAngle = BRISTOL / 1000
 
     #  Latitude: 51 degrees 28' 42.7" N
@@ -19,26 +18,19 @@ class Compass:
 
     def __init__(self):
         XLoBorg.Init()
-
-    def get_compass_values_raw(self):
-        return XLoBorg.ReadCompassRaw()
+        self.needle = XLoBorg
 
     def get_heading(self):
         #TODO I think I'm using the wrong x,y,z  fix that.
         # Read and render the raw compass readings, correcting by our manual calibration values
-        compass = XLoBorg.ReadCompassRaw()
-        print str(compass[0])+","+str(compass[1])+","+str(compass[2])
-        mX = compass[0] - 392.275
-        mY = compass[1] - 1014.4
-        mZ = compass[2] - 2218.6
+        readings = self.needle.ReadCompassRaw()
+        mX = readings[0] - 392.275
+        mY = readings[1] - 1014.4
+        mZ = readings[2] - 2218.6
         print "mX:"+str(mX)+",mY:"+str(mY)
 
         # Read the raw accelerometer readings
-        accel = XLoBorg.ReadAccelerometer()
-        print str(accel[0])+","+str(accel[1])+","+str(accel[2])
-        aX = accel[0]
-        aY = accel[1]
-        aZ = accel[2]
+        accel_x, accel_y, accel_z = self.needle.ReadAccelerometer()
 
         # some notes on tilt compensation
         # http://www.loveelectronics.co.uk/Tutorials/13/tilt-compensated-compass-arduino-tutorial
@@ -46,17 +38,17 @@ class Compass:
         # =======================================================================================
         # The following code does NOT yet work... it should but my maths is a bit screwy
         # When I manage to fix it then we'll have a compass that copes with being tilted, huzzah!
-        rollRadians = asin(aY)
-        pitchRadians = asin(aX)
+        rollRadians = asin(accel_y)
+        pitchRadians = asin(accel_x)
         # up to 40 degree tilt max:
         # http://www.loveelectronics.co.uk/Tutorials/13/tilt-compensated-compass-arduino-tutorial
         cosRoll = cos(rollRadians)
         sinRoll = sin(rollRadians)
-        cosPitch = cos(pitchRadians);
-        sinPitch = sin(pitchRadians);
+        cosPitch = cos(pitchRadians)
+        sinPitch = sin(pitchRadians)
 
-        vX = mX * cosPitch + mZ * sinPitch;
-        vY = mX * sinRoll * sinPitch + mY * cosRoll - mZ * sinRoll * cosPitch;
+        vX = mX * cosPitch + mZ * sinPitch
+        vY = mX * sinRoll * sinPitch + mY * cosRoll - mZ * sinRoll * cosPitch
     # =======================================================================================
 
         # get the heading in radians
@@ -66,15 +58,15 @@ class Compass:
         heading += self.declinationAngle
 
         # Correct negative values
-        if (heading  < 0):
+        if heading < 0:
             heading = heading + (2 * pi)
 
         # Check for wrap due to declination and compensate
-        if(heading > 2*pi):
+        if heading > 2*pi:
             heading -= 2*pi
 
         # convert to degrees
-        heading = heading * 180/pi;
+        heading = heading * 180/pi
 
         # get the base degrees
         heading = floor(heading)
@@ -82,6 +74,15 @@ class Compass:
         print("Current heading: %s" % heading)
         return heading
 
+    def which_way_to_bearing(self, desired_heading):
+        current_bearing = self.get_heading()
+        turn = desired_heading - current_bearing
+        alternative_turn = desired_heading - current_bearing + 360
+        if abs(turn) > abs(alternative_turn):
+            best_turn = alternative_turn
+        else:
+            best_turn = turn
+        return best_turn
 
 if __name__ == "__main__":
     compass = Compass()
