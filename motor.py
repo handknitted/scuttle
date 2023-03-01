c71d06673b960485ef5d806d3441104d720086b6
David C Kennedy <david.kennedy@mailfence.com>
2023-03-01 21:27:32 +0000

    Change motor power to 50% duty cycle via PWM

--- motor.py
+++ motor.py
@@ -12,21 +12,35 @@ _MOTOR_2_NEG = 10
 
 
 MOTOR_PINS = (_MOTOR_1_POS, _MOTOR_1_NEG, _MOTOR_2_POS, _MOTOR_2_NEG)
+PWM_PINS = []
 
-FORWARD = (1, 0, 1, 0)
-BACKWARD = (0, 1, 0, 1)
-SPIN_LEFT = (0, 1, 1, 0)
-SPIN_RIGHT = (1, 0, 0, 1)
+
+
+# How many times to turn the pin on and off each second
+Frequency = 200
+# How long the pin stays on each cycle, as a percent
+duty_cycle = 50
+
+# Setting the duty cycle to 0 means the motors will not turn
+Stop = 0
+
+FORWARD = (duty_cycle, 0, duty_cycle, 0)
+BACKWARD = (0, duty_cycle, 0, duty_cycle)
+SPIN_LEFT = (0, duty_cycle, duty_cycle, 0)
+SPIN_RIGHT = (duty_cycle, 0, 0, duty_cycle)
 
 
 def setup_gpio():
     for pin in MOTOR_PINS:
         GPIO.setup(pin, GPIO.OUT)
+        pwm_pin = GPIO.PWM(pin, Frequency)
+        pwm_pin.start(Stop)
+        PWM_PINS.append(pwm_pin)
 
 
 def motors_on(movement):
-    for index, pin in enumerate(MOTOR_PINS):
-        GPIO.output(pin, movement[index])
+    for index, pin in enumerate(PWM_PINS):
+        pin.changeDutyCycle(movement[index])
 
 
 def motors_off():
