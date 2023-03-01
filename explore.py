f2b2e5fb3c3788dc8eea148bd69a7a593400029b
David C Kennedy <david.kennedy@mailfence.com>
2023-03-01 18:45:54 +0000

    Handle exit and stop motors

--- explore.py
+++ explore.py
@@ -1,19 +1,27 @@
+import atexit
+
 import mag3110
 from distance import get_distance
 from motor import FORWARD, BACKWARD, SPIN_RIGHT, SPIN_LEFT, motors_on, motors_off
 
 minimum_distance = 0.3
 
+atexit.register()
+
 
 def is_clear_forward():
     return get_distance() > minimum_distance
 
 
-while True:
-    # check distance in front
-    if is_clear_forward():
-        # go straight
-        motors_on(FORWARD)
-    else:
-        # try turning
-        motors_on(SPIN_LEFT)
+try:
+    while True:
+        # check distance in front
+        if is_clear_forward():
+            # go straight
+            motors_on(FORWARD)
+        else:
+            # try turning
+            motors_on(SPIN_LEFT)
+except KeyboardInterrupt:
+    motors_off()
+    raise SystemExit
