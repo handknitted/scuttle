e29c1954ac660b04dc63e7e40eb74a87f2a1dce3
David C Kennedy <david.kennedy@mailfence.com>
2023-03-03 20:09:34 +0000

    Use the new class methods to change movement

--- explore.py
+++ explore.py
@@ -1,17 +1,7 @@
-import atexit
 import logging
-from enum import Enum
 import time
 from distance import DistanceSensor, CheckTooSoon
-from motor import FORWARD, BACKWARD, SPIN_RIGHT, SPIN_LEFT, motors_on, motors_off
-
-
-class Movement(Enum):
-
-    Forward = FORWARD
-    Backward = BACKWARD
-    SpinLeft = SPIN_LEFT
-    SpinRight = SPIN_RIGHT
+from motor import ScuttleMotors
 
 
 logging.basicConfig(level=logging.INFO)
@@ -20,10 +10,8 @@ minimum_distance = 0.3
 
 distance_sensor = DistanceSensor(warning_distance=minimum_distance)
 
-
+scuttle = ScuttleMotors()
 try:
-    current_movement = None
-    new_movement = None
 
     while True:
         # check distance in front
@@ -31,20 +19,17 @@ try:
             if distance_sensor.is_clear_forward():
                 logging.info("Clear forward")
                 # go straight
-                new_movement = Movement.Forward
+                new_movement = scuttle.forward()
             else:
                 logging.info("Obstacle within %sm. Turning." % minimum_distance)
                 # try turning
-                new_movement = Movement.SpinLeft
-                motors_on(new_movement.value)
+                new_movement = scuttle.spin_left()
 
-            if current_movement != new_movement:
-                motors_on(new_movement.value)
         except CheckTooSoon as e:
             # ignore and loop again
             time.sleep(0.05)
             pass
 except KeyboardInterrupt:
 
-    motors_off()
+    scuttle.stop()
     raise SystemExit
