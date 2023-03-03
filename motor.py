43c8b3a32d6f38b3e9dc563af0be854bc4439aaa
David C Kennedy <david.kennedy@mailfence.com>
2023-03-03 20:09:06 +0000

    Refactored to class for scuttle, encapsulate

--- motor.py
+++ motor.py
@@ -1,58 +1,103 @@
 import RPi.GPIO as GPIO
+from threading import Thread
 
 
-# Set the GPIO modes
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+class Motors(object):
 
-_MOTOR_1_POS = 7
-_MOTOR_1_NEG = 8
-_MOTOR_2_POS = 9
-_MOTOR_2_NEG = 10
+    pass
 
 
-MOTOR_PINS = (_MOTOR_1_POS, _MOTOR_1_NEG, _MOTOR_2_POS, _MOTOR_2_NEG)
-PWM_PINS = []
+class ScuttleMotors(Motors):
 
+    def __init__(self):
+        self.old_movement = None
+        # Set the GPIO modes
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
+        _MOTOR_1_POS = 7
+        _MOTOR_1_NEG = 8
+        _MOTOR_2_POS = 9
+        _MOTOR_2_NEG = 10
 
-# How many times to turn the pin on and off each second
-Frequency = 200
-# How long the pin stays on each cycle, as a percent
-duty_cycle = 50
+        motor_pins = (_MOTOR_1_POS, _MOTOR_1_NEG, _MOTOR_2_POS, _MOTOR_2_NEG)
+        self.PWM_PINS = []
 
-# Setting the duty cycle to 0 means the motors will not turn
-Stop = 0
+        # How many times to turn the pin on and off each second
+        frequency = 200
+        # How long the pin stays on each cycle, as a percent
+        duty_cycle = 50
 
-FORWARD = (duty_cycle, 0, duty_cycle, 0)
-BACKWARD = (0, duty_cycle, 0, duty_cycle)
-SPIN_LEFT = (0, duty_cycle, duty_cycle, 0)
-SPIN_RIGHT = (duty_cycle, 0, 0, duty_cycle)
+        # Setting the duty cycle to 0 means the motors will not turn
+        stop = 0
 
+        self.FORWARD = (duty_cycle, 0, duty_cycle, 0)
+        self.BACKWARD = (0, duty_cycle, 0, duty_cycle)
+        self.SPIN_LEFT = (0, duty_cycle, duty_cycle, 0)
+        self.SPIN_RIGHT = (duty_cycle, 0, 0, duty_cycle)
+        self.STOP = (0, 0, 0, 0)
 
-def setup_gpio():
-    for pin in MOTOR_PINS:
-        GPIO.setup(pin, GPIO.OUT)
-        pwm_pin = GPIO.PWM(pin, Frequency)
-        pwm_pin.start(Stop)
-        PWM_PINS.append(pwm_pin)
+        for pin in motor_pins:
+            GPIO.setup(pin, GPIO.OUT)
+            pwm_pin = GPIO.PWM(pin, frequency)
+            pwm_pin.start(stop)
+            self.PWM_PINS.append(pwm_pin)
 
+    def motors_change(self, move):
+        # if we have no old movement then just start
+        if self.old_movement is None:
+            for index, pin in enumerate(self.PWM_PINS):
+                pin.ChangeDutyCycle(move[index])
+        else:
+            # otherwise create threads to change the duty cycle smoothly
+            running_threads = []
+            for index, pin in enumerate(self.PWM_PINS):
+                pass
+                # create a thread for each pin to transition from old duty cycle to new duty cycle
+                old_duty_cycle = self.old_movement[index]
+                new_duty_cycle = move[index]
+                thread = Thread(target=self.change_duty_cycle, args=[pin, old_duty_cycle, new_duty_cycle])
+                running_threads.append(thread)
+                thread.start()
+            for thread in running_threads:
+                thread.join()
 
-def motors_on(movement):
-    for index, pin in enumerate(PWM_PINS):
-        pin.ChangeDutyCycle(movement[index])
+    @staticmethod
+    def change_duty_cycle(pin, old_duy_cycle, new_duty_cycle):
+        steps = 10
+        # establish the difference in the duty cycles
+        transition_size = new_duty_cycle - old_duy_cycle
+        step_size = transition_size/steps
+        # loop with a delay, stepping the duty cycle toward the new value evenly
+        while steps > 0:
+            steps -= 1
+            old_duy_cycle += step_size
+            pin.ChangeDutyCycle(old_duy_cycle)
+            time.sleep(0.01)
 
+    def forward(self):
+        self.motors_change(self.FORWARD)
 
-def motors_off():
-    GPIO.cleanup()
+    def backward(self):
+        self.motors_change(self.BACKWARD)
 
+    def spin_left(self):
+        self.motors_change(self.SPIN_LEFT)
 
-setup_gpio()
+    def spin_right(self):
+        self.motors_change(self.SPIN_RIGHT)
+
+    def stop(self):
+        self.motors_change(self.STOP)
+
 
 if __name__ == '__main__':
     import time
-    for movement in (FORWARD, SPIN_LEFT, BACKWARD, SPIN_RIGHT):
-        motors_on(movement)
+    scuttle = ScuttleMotors()
+    for movement in (scuttle.forward,
+                     scuttle.spin_left,
+                     scuttle.backward,
+                     scuttle.spin_right,
+                     scuttle.stop):
+        movement()
         time.sleep(1)
-
-    motors_off()
